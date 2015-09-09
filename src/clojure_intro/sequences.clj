9becca36e9e6dc3245a648a8545113c746bdc873
Antanas Bastys <antanas.bastys@tieto.com>
2015-09-09 21:11:46 +0300

    Removed nont needed impl

--- src/clojure_intro/sequences.clj
+++ src/clojure_intro/sequences.clj
@@ -5,13 +5,13 @@
 ;(sum-fn inc identity 5) ;=> 11
 ;(sum-fn identity - 10) ;=> 0
 (defn sum-fn [f g x]
-  (+ (f x) (g x)))
+  nil)
 
 ;(filter (equal-to 2) [2 1 3 2.0])    ;=> (2 2.0)
 ;(filter (equal-to 2) [3 4 5 6])
 ;Hint use fn or #()
 (defn equal-to-fn [n]
-  (fn [k] (= k n)))
+  nil)
 
 ;(filter (set->predicate #{1 2 3})     [0 2 4 6])       ;=> (2)
 ;(filter (set->predicate #{1 2 3 nil}) [2 nil 4 nil 6]) ;=> (2 nil nil)
@@ -19,19 +19,19 @@
 (defn set->predicate [a-set]
   "Given a set returns fn of single x argument
   that returns true if x is in the set"
-  (fn [x] (contains? a-set x)))
+  nil)
 
 
 (defn first-not-null [& args]
   "Returns first not null argument or null"
-  (some identity args))
+  nil)
 
 ;(filter (pred-and pos? even?) [1 2 -4 0 6 7 -3]) => [2 6]
 ;(filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3]) ;=> [1 7]
 ;(filter (pred-and (complement nil?) empty?) [[] '() nil {} #{}]) => [[] '() {} #{}]
 (defn pred-and [pred1 pred2]
   "Given 2 single ar predicate returns 'and' predicate"
-  (fn [x] (and (pred1 x) (pred2 x))))
+  nil)
 
 ;From Apache Commons
 ;public static boolean isWhitespace(CharSequence cs) {
@@ -55,43 +55,43 @@
 (defn- is-whitespace? [char] (Character/isWhitespace char))
 (defn blank? [s]
   "Tells if string s contains only blanks"
-  (empty? (remove is-whitespace? s)))
+  nil)
 
 (defn give-me-odds [& args]
   "Returns only odd numbers from given arguments"
-  (filter odd? args))
+  nil)
 
 (defn give-me-first-5 [& args]
   "Returns first 5 arguments"
-  (take 5 args))
+  nil)
 
 (defn give-me-distinct [& args]
   "Returns only distinct arguments"
-  (distinct args))
+  nil)
 
 ;(append-and-concat 1 [2 3 4] [5 6 7] => [1 2 3 4 5 6 7]
 (defn append-and-concat [elem coll1 coll2]
   "Appends elem to begining of 2 concated collections"
-  (cons elem (concat coll1 coll2)))
+  nil)
 
 ;Don't try running (repeat "a") in REPL :)
 (defn duplicate-five-times [item]
   "Repeates item five times"
-  (repeat 5 item))
+  nil)
 
 ;Just flatten it damn it
 (defn good-old-flatten [coll]
-  (flatten coll))
+  nil)
 
 (defn is-all-even? [coll]
   "Returns true if all numbers in collection are even"
-  (every? even? coll))
+  nil)
 
 ;Removes empty collections from collection of collections
 (defn not-empty-colls [coll-of-colls]
-  (remove empty? coll-of-colls))
+  nil)
 
 ;factorial without recursion nor iteration
 ;Hint (range 1 5) => (1 2 3 4), Use apply or reduce
 (defn no-iteration-factorial [n]
-  (apply * (range 1 (inc n))))
+  nil)
