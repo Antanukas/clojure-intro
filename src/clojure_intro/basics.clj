7bf9da656a3fa902fb682248453c36159ba01f11
Antanas Bastys <antanas.bastys@tieto.com>
2015-09-10 19:12:54 +0300

    Comments

--- src/clojure_intro/basics.clj
+++ src/clojure_intro/basics.clj
@@ -1,11 +1,14 @@
 (ns clojure-intro.basics)
 
-;numerics; DO NOT USE use odd?
+;you can use anything if I haven added comment about restrictions
+
+;numerics; DO NOT USE odd?
+;Fns mod,=
 (defn is-odd? [x]
   nil)
 
 (defn sum [x y]
-  nil)
+  (+ x y))
 
 (defn subtr [x y]
   nil)
@@ -16,58 +19,73 @@
 (defn multiply [& args]
   nil)
 
+;Inc
 (defn increase-by-one [x]
   nil)
 
 ;remember str?
+;Hint str
 (defn concat-two-strings [s1 s2]
   nil)
 
+;apply, str, reverse
 ;reverse a string
 (defn my-reverse [s1]
   nil)
 
 ;booleans
+;true?
 (defn is-true? [x]
   nil)
 
+;not
 (defn my-not [x]
   nil)
 
+;and
 (defn my-and [x y]
   nil)
 
+;or
 (defn my-or [x y]
   nil)
 
+;nil?
 (defn my-is-nil? [x]
   nil)
 
 ;Collections
+;[]
 (defn give-me-some-vector []
   nil)
 
-;TODO comma thing
+;'()
 (defn give-me-some-list []
   nil)
 
+;hashmap, {}, sorted-map
 (defn give-me-some-maps []
   ;return vector containing hashmaps and sorted maps
   nil)
 
+;#{}, hash-set, sorted-set
 (defn give-me-some-sets []
   ;return vector containing hashsets and sorted sets
   nil)
 
+;apply list cons
 (defn make-list [e1 & rest]
   nil)
 
+;vec
 (defn make-vector [e1 & rest]
   nil)
 
+;hash-map
 (defn make-hash-map [& entries]
   nil)
 
+;set
 (defn make-set [& values]
   nil)
 
